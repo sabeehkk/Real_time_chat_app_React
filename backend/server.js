a2171d13d143cf32185f3c9f663042cc505bf5aa
sabeehkk <sabeehkadungalloor573@gmail.com>
2024-03-05 00:35:55 +0530

    finished chat application

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
@@ -11,6 +12,9 @@ import { app, server } from "./socket/socket.js"
 
 
 const PORT =process.env.PORT || 5000;
+
+const __dirname = path.resolve()
+
 dotenv.config()
 app.use(express.json())
 app.use(cookieParser())
@@ -18,9 +22,10 @@ app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes)
 app.use("/api/users",userRoutes)
 
-// app.get("/", (req, res) => {
-//     res.send("hello world")
-// })
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 server.listen(PORT,()=>{
     connectMongoDb()
