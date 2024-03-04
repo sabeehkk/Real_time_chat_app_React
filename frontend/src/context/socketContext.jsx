81ac2e95ad0dbd63e539710b5f6c9c670a946043
sabeehkk <sabeehkadungalloor573@gmail.com>
2024-03-05 01:22:06 +0530

    socket url updated

--- frontend/src/context/socketContext.jsx
+++ frontend/src/context/socketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider =({children})=>{
 
     useEffect(()=>{
         if(authUser){
-            const socket =io("http://localhost:5000",{
+            const socket =io("https://chat-app-0v8e.onrender.com/",{
                 query:{
                     userId:authUser._id,
                 }
